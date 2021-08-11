a23011d50300f55a7abd1c9d01b3271f2452043f
Savionw1998 <winstonsavion@github.com>
2021-08-11 16:52:33 -0700

    button Click

--- kivy-thelab/main.py
+++ kivy-thelab/main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+
+class WidgetsExample(GridLayout):
+    my_text = StringProperty("Hello!")
+    def on_button_click(self):
+        print("Button Clicked!")
+        self.my_text = "You Clicked!"
 
 
 class StackLayoutExample(StackLayout):
