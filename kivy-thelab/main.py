67558d76b4ad939e0975cac6b7e0a8e2e0b33e05
Savionw1998 <winstonsavion@github.com>
2021-08-12 13:41:40 -0700

    Current Progress

--- kivy-thelab/main.py
+++ kivy-thelab/main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -10,18 +10,23 @@ from kivy.uix.widget import Widget
 
 class WidgetsExample(GridLayout):
     count = 1
+    count_enabled = BooleanProperty(False)
     my_text = StringProperty("0")
+
     def on_button_click(self):
         print("Button Clicked!")
-        self.my_text = str(self.count)
-        self.count += 1
+        if self.count_enabled:
+            self.count += 1
+            self.my_text = str(self.count)
+
     def on_toggle_state(self, widget):
         print("Toggle State!" + widget.state)
         if widget.state == "normal":
             widget.text = "OFF"
+            self.count_enabled = False
         else:
             widget.text = "ON"
-
+            self.count_enabled = True
 
 
 class StackLayoutExample(StackLayout):
