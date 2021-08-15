8c812d8e014d103a81571e45ea919c994cffc7b6
Savionw1998 <winstonsavion@gmail.com>
2021-08-15 08:55:49 -0700

    Programming Hub Course Content

--- Kivy-PHub/main.py
+++ Kivy-PHub/main.py
@@ -9,13 +9,13 @@ class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text="[u][b]Name: [/b][/u]", font_size=20, markup=True))
+        self.add_widget(Label(text="[u][b]NAME: [/b][/u]", font_size=20, markup=True))
         self.name = TextInput(multiline=False)
         self.add_widget(self.name)
-        self.add_widget(Label(text="[i]Email: [/i]", font_size=25, markup=True))
+        self.add_widget(Label(text="[i]EMAIL: [/i]", font_size=25, markup=True))
         self.name = TextInput(multiline=False)
         self.add_widget(self.name)
-        self.add_widget(Label(text="[color=F2DAA9] Password: [/color]", font_size=30, markup=True))
+        self.add_widget(Label(text="[color=F2DAA9] PASSWORD: [/color]", font_size=30, markup=True))
         self.name = TextInput(multiline=False)
         self.add_widget(self.name)
 
